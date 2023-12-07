cec21ca20df6dc2d6152b7967d4c89cfd8e99b88
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2023-12-07 21:40:01 +0100

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,9 @@
     import HeroCard from "./HeroCard.svelte";
 </script>
 <div class="h-screen w-screen flex flex-row justify-center items-center text-7xl space-x-20">
-    <HeroCard ></HeroCard>
-    <HeroCard></HeroCard>
-    <HeroCard></HeroCard>
+    <HeroCard name="Politicien" desc="Mentez pour gagner" logoRoute="../public/politician.png"></HeroCard>
+    <HeroCard name="Personnes agées" desc="Vous êtes à la retraite" logoRoute="../public/old.png"></HeroCard>
+    <HeroCard name="Personnes agées" desc="Vous êtes à la retraite" logoRoute="../public/old.png"></HeroCard>
 </div>
 
 <style>
--- src/HeroCard.svelte
+++ src/HeroCard.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-    let name : string = "Politicien";
-    let desc : string = "Mentez pour gagner";
-    import logo from './assets/politician.png';
+    export let name : string;
+    export let desc : string;
+    export let logoRoute : string;
+
 
 </script>
 
 <div class="w-1/5">
     <div class="p-4 w-full h-full card bg-white shadow-xl rounded-xl divide-y-2 hover:transition-all hover:cursor-pointer hover:scale-105">
-        <figure class="p-10"><img class="w-42" src={logo} alt="Shoes" /></figure>
+        <figure class="p-10"><img class="w-42" src={logoRoute} alt="Shoes" /></figure>
         <div>
             <h2 class="card-title text-3xl pt-2 justify-center divide-y">{name}</h2>
             <div class="card-body">
