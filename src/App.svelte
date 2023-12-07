79b1680699adbb70fa58f7fc2a55d1e5a306c34d
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2023-12-07 21:51:44 +0100

    choiceCard

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
     import HeroCard from "./HeroCard.svelte";
+    import ChoiceCard from "./ChoiceCard.svelte";
 </script>
 <div class="h-screen w-screen flex flex-row justify-center items-center text-7xl space-x-20">
     <HeroCard name="Politicien" desc="Mentez pour gagner" logoRoute="../public/politician.png"></HeroCard>
     <HeroCard name="Personnes agées" desc="Vous êtes à la retraite" logoRoute="../public/old.png"></HeroCard>
     <HeroCard name="Ecolo" desc="Vous aimez l'herbe" logoRoute="../public/farmer.png"></HeroCard>
+    <ChoiceCard name="Velo" logoRoute="../public/farmer.png"></ChoiceCard>
 </div>
