b1ef0c26fb9ebb02fad04442954799114cfd81c6
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2023-12-08 03:59:58 +0100

    t

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
 
-    import {Route, Router} from "svelte-routing";
+    import {navigate, Route, Router} from "svelte-routing";
     import Dashboard from "./components/dashboard/Dashboard.svelte";
     import Homepage from "./Homepage.svelte";
+    import {onMount} from "svelte";
+
+    let inputText = "";
+    let typingTimeout: NodeJS.Timeout | null = null;
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        const pressedKey = event.key.toLowerCase();
+        inputText += pressedKey;
+
+        if (typingTimeout) {
+            clearTimeout(typingTimeout);
+        }
+
+        typingTimeout = setTimeout(() => {
+            if (inputText === "emoji") {
+                //@ts-ignore
+                emojisplosions()
+            }
+            if(inputText === "rick"){
+                navigate("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {replace: true})
+            }
+
+            inputText = ""; // Réinitialisez le texte après le délai
+        }, 1000);
+    };
+
+    onMount(() => {
+        document.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    });
+
 </script>
 
 
