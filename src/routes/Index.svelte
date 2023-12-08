36075a41c51c323f0d0830ca6a959e11d141422e
Elyroma <elynebeaudeau@orange.fr>
2023-12-08 03:54:29 +0100

    fix

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -33,14 +33,14 @@
 {:else}
     {#if title !== ""}
         <div class="w-1/3 h-36 absolute top-5 left-5 flex">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="hover:cursor-pointer mr-3" on:click={goBack}><path fill="currentColor" d="M22 12a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2a10 10 0 0 1 10 10M7 12l5 5v-3h4v-4h-4V7l-5 5Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="hover:cursor-pointer mr-3" on:click={goBack}><path fill="white" d="M22 12a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2a10 10 0 0 1 10 10M7 12l5 5v-3h4v-4h-4V7l-5 5Z"/></svg>
             <DescriptionCard title={title} description={description}/>
         </div>
     {/if}
 
     <div class="w-full absolute bottom-10 flex flex-row gap-6 justify-center p-8">
         {#each responses as response}
-            <div class="h-36" on:click={() => myChoice = response._id}>
+            <div class="h-36" on:click={() => myChoice = response.id}>
                 <ChoiceCard name={response.title} logoRoute={response.icon}/>
             </div>
         {/each}
---
+++ src/routes/Velo.svelte
@@ -0,0 +1,10 @@
+<div class="bg-velo h-full w-full">
+</div>
+
+<style>
+    .bg-velo {
+        background-image: url("../../public/velo.jpg");
+        background-position: 50%;
+        background-size: cover;
+    }
+</style>
