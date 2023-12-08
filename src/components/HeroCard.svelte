02c68f99eacdba23340a3f6513bc72d409c6e135
Mathis Mazoyer <mathis.mazoyer@etu.univ-smb.fr>
2023-12-08 01:53:34 +0100

    fix: img

--- src/components/HeroCard.svelte
+++ src/components/HeroCard.svelte
@@ -3,7 +3,7 @@
     export let desc : string = "";
     export let logoRoute : string = "";
 </script>
-
+ 
 <div class="">
     <div class="p-4 w-full h-full card bg-white shadow-xl rounded-xl divide-y-2 hover:transition-all hover:cursor-pointer hover:scale-105">
         <figure class="p-10"><img class="w-42" src={logoRoute} alt="Shoes" /></figure>
--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -1,7 +1,7 @@
 import type IHero from "../interfaces/IHero";
 
 export const HEROS: IHero[] = [
-    {name: "Politicien", desc: "Mentez pour gagner", logoRoute: "../public/politician.png"},
-    {name: "Personnes agées", desc: "Vous êtes à la retraite", logoRoute: "../public/old.png"},
-    {name: "Ecolo", desc: "Vous aimez l'herbe", logoRoute: "../public/farmer.png"}
+    {name: "Politicien", desc: "Mentez pour gagner", logoRoute: "politician.png"},
+    {name: "Personnes agées", desc: "Vous êtes à la retraite", logoRoute: "old.png"},
+    {name: "Ecolo", desc: "Vous aimez l'herbe", logoRoute: "farmer.png"}
 ];
