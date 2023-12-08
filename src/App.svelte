07ad9719d41997d5f1fd9423ae447379af0f6b60
Elyroma <elynebeaudeau@orange.fr>
2023-12-08 01:09:49 +0100

    background change

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+    import {snow} from "./components/snow";
     import {onMount} from "svelte";
     import Index from "./route/Index.svelte";
-    import Maison from "./route/Maison.svelte";
     import type IHero from "./interfaces/IHero";
     import type IResponse from "./interfaces/IResponse";
     import type IStory from "./interfaces/IStory";
     import Story from "./services/Story";
+    import Start from "./route/Start.svelte";
 
     let allStory: IStory[] = [];
 
@@ -17,6 +18,7 @@
     let title: string = "Bienvenue !";
     let desc: string = "En tant que ???, vous allez devoir agir pour faire face au changement climatique.";
     let responses: IResponse[] = [{_id: "1", title: "Suivant"}];
+    let background: any = Start;
 
     function update(): void {
         desc = "En tant que " + hero?.name + ", vous allez devoir agir pour faire face au changement climatique.";
@@ -44,12 +46,17 @@
         allStory = await (Story.getAll());
     })
 
+    window.onload = function(){
+        snow.init(100); // 10 flocons seront donc affichés
+    };
+
     $: hero && update();
     $: myChoice && next();
 </script>
 
 <Index bind:myHero={hero} bind:myChoice={myChoice} title={currentStory?.title ?? title} description={currentStory?.description ?? desc} responses={currentStory?.responses ?? responses} goBack={goBack}>
     <div slot="page" class="h-full w-full">
-        <Maison/>
+        <svelte:component this={(currentStory?.page ?? background)}></svelte:component>
     </div>
 </Index>
+
---
+++ src/route/Start.svelte
@@ -0,0 +1,11 @@
+<div class="h-full w-full hp-hero">
+    <figure class="absolute bottom-0 left-3"><img class="w-42" src="../../public/hero-illustrations.png" alt="Shoes" /></figure>
+</div>
+
+<style>
+    .hp-hero {
+        background-image: url("../../public/micro-pattern.png"),url("../../public/hero-bkg-trsp.png"),linear-gradient(-20deg,#4dab7f,#047385 100%);
+        background-position: 50%;
+        background-size: auto,cover,auto;
+    }
+</style>
